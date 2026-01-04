0e86e184b4d43ded9d998fb0e1656da1b1840897
Aura <inverno502@gmail.com>
2026-01-04 22:23:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, send_from_directory
+import os
 
 app = Flask(__name__, static_folder='.', static_url_path='')
 
@@ -7,4 +8,5 @@ def index():
     return send_from_directory('.', 'BemVindoAura.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
