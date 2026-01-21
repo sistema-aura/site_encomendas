db502367fdf0428e6299d5ca5a3f5433711fe6d8
Aura <inverno502@gmail.com>
2026-01-21 11:42:12 +0000

    Create MATERIAIS_POR_PRODUTO.js

---
+++ js/MATERIAIS_POR_PRODUTO.js
@@ -0,0 +1,69 @@
+const MATERIAIS_POR_PRODUTO = {
+  "ATI FX45": {
+    "M1911": 1,
+    "Fios de Cobre": 2,
+    "Tubo de Plástico": 2,
+    "Plástico": 75,
+    "Borracha": 75,
+    "Alumínio": 50,
+    "Upgrade Pistolas": 1,
+    "Cobre": 50,
+    "Vidro": 75
+  },
+  "Empunhadura": {
+    "Plástico": 25,
+    "Fita Adesiva": 1,
+    "Componentes Eletrônicos": 1,
+    "Borracha": 25
+  },
+  "Mira Holográfica": {
+    "Bateria AA+": 1,
+    "Fita Adesiva": 1,
+    "Vidro": 25,
+    "Plástico": 25
+  },
+  "Lanterna": {
+    "Bateria AA": 1,
+    "Fita Isolante": 1,
+    "Vidro": 25,
+    "Plástico": 25
+  },
+  "M1911": {
+    "Corpo de Pistola": 1,
+    "Borracha": 175,
+    "Plástico": 175,
+    "Cobre": 150,
+    "Alumínio": 150,
+    "Peças de Armas": 3,
+    "Vidro": 175
+  },
+  "Munição de Pistola": {
+    "Frasco de Pólvora": 3,
+    "Cobre": 15
+  },
+  "Munição de Sub": {
+    "Frasco de Pólvora": 5,
+    "Cobre": 15,
+    "Alumínio": 15
+  },
+  "Munição de Fuzil": {
+    "Frasco de Pólvora": 8,
+    "Cobre": 30,
+    "Alumínio": 30
+  },
+  "Pente Estendido": {
+    "Fita Isolante": 2,
+    "Fita Adesiva": 2,
+    "Plástico": 50,
+    "Borracha": 50,
+    "Alumínio": 35
+  },
+  "Silenciador": {
+    "Cobre": 25,
+    "Pasta de Dente": 2,
+    "Fita Isolante": 1,
+    "Fita Adesiva": 1,
+    "Garrafa Vazia": 1,
+    "Plástico": 50
+  }
+};
